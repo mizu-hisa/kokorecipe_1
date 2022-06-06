d9b12ef90c485c4c5e57a8c25134ba32191fc9cb
mizu-hisa <101858095+mizu-hisa@users.noreply.github.com>
2022-06-06 14:41:16 +0000

    ログイン・サインアップ

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   }
 
   # root to: 'homes/top' => 'homes#top'
-  get '/top' => 'homes#top'
+  root to: "homes#top"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
